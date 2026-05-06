4c6f8e9a398938725d686d8b2085801efc6b5f03
stephenJoshua <149865909+stephen1234567890000000@users.noreply.github.com>
2026-05-06 13:35:48 +0700

    feat: add complete enum status

--- routes/booking.router.js
+++ routes/booking.router.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const router = express.Router();  
+const router = express.Router();
 const BookingController = require("../controller/booking.controller");
 const authMiddleware = require("../middleware/authentication.middleware");
+const authorizedAdmin = require("../middleware/authoritazion.middleware");
 
-router.post("/", authMiddleware, BookingController.createBooking);
 router.get("/my-bookings", authMiddleware, BookingController.getMyBookings);
+router.get(
+  "/admin/all",
+  authMiddleware,
+  authorizedAdmin,
+  BookingController.findAllByAdmin,
+);
+router.post("/", authMiddleware, BookingController.createBooking);
+
+router.patch("/:id/cancel", authMiddleware, BookingController.cancelBooking);
+router.patch("/:id/reSchedule", authMiddleware, BookingController.reSchedule);
+router.patch("/:id/complete", authMiddleware, BookingController.complete);
 
 module.exports = router;
--- services/booking.service.js
+++ services/booking.service.js
@@ -1,6 +1,7 @@
 const { where } = require("sequelize");
-const { Booking, Schedule } = require("../models/index");
+const { Booking, Schedule, User, sequelize } = require("../models/index");
 const { Op } = require("sequelize");
+const { lock } = require("../routes");
 
 class BookingService {
   static async createBooking(UserId, ScheduleId) {
@@ -68,7 +69,7 @@ class BookingService {
         {
           UserId,
           ScheduleId,
-          status: "pending",
+          status: "confirmed",
         },
         { transaction },
       );
@@ -81,6 +82,233 @@ class BookingService {
       await transaction.rollback();
       throw error;
     }
+  }
+
+  static async getMyBookings(UserId) {
+    const myBookings = await Booking.findAll({
+      where: { UserId },
+      include: [
+        {
+          model: Schedule,
+          include: [
+            {
+              model: User,
+              attribute: ["id", "name", "email"],
+            },
+          ],
+        },
+      ],
+      order: [[{ model: Schedule }, "date", "ASC"]],
+      order: [[{ model: Schedule }, "startTime", "ASC"]],
+    });
+
+    // return myBookings;
+
+    const formatted = myBookings.map((b) => ({
+      id: b.id,
+      status: b.status,
+      date: b.Schedule.date,
+      startTime: b.Schedule.startTime,
+      endTime: b.Schedule.endTime,
+      trainerName: b.Schedule.User.name,
+    }));
+
+    return formatted;
+  }
+
+  static async cancelBooking(UserId, bookingId) {
+    const transaction = await sequelize.transaction();
+    try {
+      const booking = await Booking.findByPk(bookingId, {
+        include: Schedule,
+        transaction: transaction,
+        lock: transaction.LOCK.UPDATE,
+      });
+
+      if (!booking) {
+        throw {
+          statusCode: 404,
+          message: "Booking not found",
+        };
+      }
+
+      if (booking.UserId !== UserId) {
+        throw {
+          statusCode: 403,
+          message: "Forbidden action",
+        };
+      }
+
+      if (booking.status !== "pending") {
+        throw {
+          statusCode: 400,
+          message: "Booking status are not pending",
+        };
+      }
+
+      const scheduleTimeDate = new Date(
+        booking.Schedule.date + " " + booking.Schedule.startTime,
+      );
+
+      const now = new Date();
+
+      const diff = (scheduleTimeDate - now) / (1000 * 600 * 60);
+
+      if (diff < 2) {
+        throw {
+          statusCode: 400,
+          message: "Too late to cancel",
+        };
+      }
+
+      booking.status = "cancelled";
+      await booking.save({ transaction: transaction });
+
+      booking.Schedule.capacity += 1;
+      await booking.Schedule.save({ transaction: transaction });
+
+      await transaction.commit();
+
+      return booking;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  }
+
+  static async reSchedule(bookingId, newScheduleId, UserId) {
+    const transaction = await sequelize.transaction();
+    try {
+      const booking = await Booking.findByPk(bookingId, {
+        include: Schedule,
+        transaction: transaction,
+        lock: transaction.LOCK.UPDATE,
+      });
+
+      if (!booking) {
+        throw {
+          statusCode: 404,
+          message: "Booking not found",
+        };
+      }
+
+      if (booking.UserId !== UserId) {
+        throw {
+          statusCode: 403,
+          message: "Forbidden action",
+        };
+      }
+
+      if (booking.status !== "pending") {
+        throw {
+          statusCode: 400,
+          message: "Cannot reSchedule",
+        };
+      }
+
+      const newSchedule = await Schedule.findByPk(newScheduleId, {
+        transaction: transaction,
+        lock: transaction.LOCK.UPDATE,
+      });
+
+      if (!newSchedule) {
+        throw {
+          statusCode: 404,
+          message: "Schedule not found",
+        };
+      }
+
+      if (newSchedule.capacity <= 0) {
+        throw {
+          statusCode: 400,
+          message: "Slot full",
+        };
+      }
+
+      booking.Schedule.capacity += 1;
+      await booking.Schedule.save({ transaction: transaction });
+
+      newSchedule.capacity -= 1;
+      await newSchedule.save({ transaction: transaction });
+
+      booking.ScheduleId = newSchedule;
+      booking.status = "confirmed";
+
+      await booking.save({ transaction: transaction });
+
+      await transaction.commit();
+
+      return booking;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  }
+
+  static async findAllByAdmin() {
+    const booking = await Booking.findAll({
+      include: [
+        {
+          model: Schedule,
+          include: User,
+        },
+      ],
+      order: [[{ model: Schedule }, "date", "ASC"]],
+    });
+
+    return booking;
+  }
+
+  static async complete(bookingId, user) {
+    console.log(bookingId, 'ini booking');
+    
+    const booking = await Booking.findByPk(bookingId, {
+      include: {
+        model: Schedule,
+        include: [User],
+      },
+    });
+
+    if (!booking) {
+      throw {
+        statusCode: 404,
+        message: "Booking not found",
+      };
+    }
+
+    if (
+      user.role !== "admin" &&
+      user.id !== booking.Schedule.TrainerId
+    ) {
+      throw {
+        statusCode: 403,
+        message: "Forbidden action",
+      };
+    }
+
+    if (booking.status !== "confirmed") {
+      throw {
+        statusCode: 400,
+        message: "Booking is not confirmed",
+      };
+    }
+
+    const scheduleEnd = new Date(
+      booking.Schedule.date + " " + booking.Schedule.endTime,
+    );
+
+    const now = new Date();
+
+    if (now < scheduleEnd) {
+      throw {
+        statusCode: 400,
+        message: "Too early to complete",
+      };
+    }
+
+    booking.status = "completed";
+    await booking.save();
+    return booking;
   }
 }
 
